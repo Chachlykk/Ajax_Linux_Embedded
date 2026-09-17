5369213795099af850b1bea8495c302e92f5c9cc
Yuliia Pidopryhora <uliaa dot ouo on gmail dot com>
2026-09-17 09:30:50 +0000

    changed interaction with remote link
    fixed safety and flaws, changed config file location

--- utils/git_init.sh
+++ utils/git_init.sh
@@ -4,40 +4,58 @@ create_config(){
 	read -r -p "Enter user name:" USER_NAME
 	read -r -p "Enter user email:" USER_EMAIL
 	read -r -p "Enter default branch:" USER_BRANCH
-	read -r -p "Enter remote repository:" USER_REPOSITORY
-	cat > "$1/.git_myconfig" <<EOF
+	cat > ".git_myconfig" <<EOF
 USER_NAME="$USER_NAME"
 USER_EMAIL="$USER_EMAIL"
 USER_BRANCH="$USER_BRANCH"
-USER_REPOSITORY="$USER_REPOSITORY"
 EOF
 }
 
-if [[ -n $1 ]]; then
-
+check_dir(){
 	if git -C $1 rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-		echo -e "Git repository exists.\nExiting"
+		echo "Git repository exists."
+		if [[ -n $2 ]]; then
+			if git -C $1 remote get-url origin >/dev/null 2>&1; then
+				echo "Remote repository changed"
+				git -C $1 remote set-url origin $2
+			else
+				echo "Remote repository added"
+				git -C $1 remote add origin $2
+			fi
+		fi
+		echo "Exiting"
+		exit 0
+	elif [[ -n "$(find "$1" -mindepth 1 -maxdepth 1 -print -quit)" ]]; then
+		echo -e "Dir is not empty.\nExiting"
 		exit 1
 	fi
 
 	if [[ ! -e $1 ]];then
-		mkdir -p $1
+		mkdir -p $1	
 	fi
+}
+if [[ -n $3 ]]; then
+	echo -e "Too many parameters\nExititng"
+	exit 4
+fi
 
-	if [[ ! -f "$1/.git_myconfig" ]]; then
+if [[ -n $1 ]]; then
+	check_dir $1 $2
+
+	if [[ ! -f .git_myconfig ]]; then
 		echo "Configuration file is not found"
 		read -r -p "Create it> [y/N]" answer
 		if [[ "$answer" == "y" ]]; then
-			create_config $1
+			create_config
 		else
 			echo -e "No config file\bExiting"
 			exit 2
 		fi
 	else
-		source "$1/.git_myconfig"
+		source .git_myconfig
 	fi
 
-	if [[ -x "$USER_NAME" || -z "$USER_EMAIL" || -z "$USER_BRANCH" ]]; then
+	if [[ -z "$USER_NAME" || -z "$USER_EMAIL" || -z "$USER_BRANCH" ]]; then
 		echo "Not enough settings, check .git_myconfig file"
 		exit 3
 	fi
@@ -46,7 +64,9 @@ if [[ -n $1 ]]; then
 	git -C $1 config --local user.name "$USER_NAME"
 	git -C $1 config --local user.email "$USER_EMAIL"
 	git -C $1 config --local init.defaultBranch "$USER_BRANCH"
-	git -C $1 remote add origin "$USER_REPOSITORY"
+	if [[ -n $2 ]]; then
+		git -C $1 remote add origin $2
+	fi
 else
 	echo -e "Usage:\n$0 <directory>\n$0 <directory> <remote_utl>"
 fi
